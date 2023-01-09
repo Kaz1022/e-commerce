1107b78a6226794c9f4d378927f5fa82d91f75db
Kazuko Baynton <kazukobaynton@gmail.com>
2023-01-09 14:22:58 -0800

    refactor to react dom @6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
 // import from index.js in pages directory
@@ -8,7 +8,19 @@ import { Home, About, Products, SingleProduct, Cart, Error, Checkout, Private } 
 function App() {
   return (
     <Router>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProduct />} />
+        <Route path="checkout" element={
+          <Private>
+            <Checkout />
+          </Private>
+        } />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </Router>
   )
 }
