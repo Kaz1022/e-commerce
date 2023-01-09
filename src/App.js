462b386accdfdd51736adf3a489594f7ba52872f
Kazuko Baynton <kazukobaynton@gmail.com>
2023-01-08 16:38:06 -0800

    add route

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
+// import from index.js in pages directory
+import { Home, About, Products, SingleProduct, Cart, Error, Checkout, Private } from './pages'
+
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      
+    </Router>
+  )
 }
 
 export default App
